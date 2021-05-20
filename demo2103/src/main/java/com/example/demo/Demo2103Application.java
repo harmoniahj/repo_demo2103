170c2bc1bafeb6c155047694eb00baff9a4132ff
harmoniahj <harmoniahj@naver.com>
2021-05-20 12:00:53 +0900

    리액트 환경 추가

--- demo2103/src/main/java/com/example/demo/Demo2103Application.java
+++ demo2103/src/main/java/com/example/demo/Demo2103Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2103Application {
 
+ // Test 클래스
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2103Application.class, args);
 	}
